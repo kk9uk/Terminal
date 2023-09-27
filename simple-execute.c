738527bb0072a0f8f6881a8cda70b00d6da58419
Danielw920 <124156069+Danielw920@users.noreply.github.com>
2023-09-27 17:01:25 +0800

    I'm Daniel

--- simple-execute.c
+++ simple-execute.c
@@ -7,7 +7,7 @@
 
 // Group member 1: TSUI, Hiu Kei (1155174948)
 // Group member 2: TSANG, Cheuk Hang (1155167650)
-// Group member 3: <Daniel> (11551)
+// Group member 3: WONG, Chun Yin (1155175977)
 
 int shell_execute(char ** args, int argc)
 {
